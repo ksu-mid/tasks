6f7dffa0522fb7058ca10a639800ad66fb62c615
ksu-mid <48205739+ksu-mid@users.noreply.github.com>
2019-09-14 01:52:37 +0300

    Add files via upload

--- 9_1.php
+++ 9_1.php
@@ -5,7 +5,7 @@
     <form action="" method="GET">
         <input type="button" id="btn" value="Жми на кнопку!" onclick="show_words()">
     </form>
-    <script type="text/javascript" src="index.js"></script>
+    <script type="text/javascript" src="9_2.js"></script>
    <div id="text"> </div>
 </body>
 </html>
